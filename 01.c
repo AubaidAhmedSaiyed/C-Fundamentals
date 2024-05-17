a8b1a9d8f25388334d489c1f9abc68f29e0e968b
AubaidAhmedSaiyed <aubaidahmed6@gmail.com>
2024-05-17 13:42:39 +0530

    first c porogramn

---
+++ 01.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+int sayName(int age, char name[]){
+  // printf("Hello, World!\n");
+  // //   //here %s is a place holder which will hold the variable name
+  //   printf("heelo my name and age is %s and %d\n", name ,age);
+    
+    return age; 
+}
+
+void main()
+{
+  
+  
+  char name[]="AUBAID";
+  int age=17;
+  //   printf("Hello, World!\n");
+  //   //here %s is a place holder which will hold the variable name
+  //   printf("heelo my name and age is %s and %d\n", name ,age);
+  
+    int meriAge = sayName(age, name);
+    printf("%d", meriAge);
+    
+    
+}
